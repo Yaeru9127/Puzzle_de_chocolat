d3460af7c34ef25c0ff9a7fe24d7b32ccfd5beb2
6F\ktc <>
2025-06-11 10:46:24 +0900

    ステージ選択のシーン移動(タイトル移動も込)

---
+++ Puzzle_de_Chocolat/Assets/Mori/script/sisakuhinn/scenechange.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour
+{
+    public void change_button() 
+    {
+        Debug.Log("aaaaaa");
+        //SceneManager.LoadScene("");
+    }
+}
