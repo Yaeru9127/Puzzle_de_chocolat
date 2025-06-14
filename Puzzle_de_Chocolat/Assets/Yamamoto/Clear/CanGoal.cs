8291589ca36b22cd628cd51966e457d4b0235540
GTUNE\ktc <>
2025-06-14 10:55:10 +0900

    ゴール判定システムの完成

--- Puzzle_de_Chocolat/Assets/Yamamoto/Clear/CanGoal.cs
+++ Puzzle_de_Chocolat/Assets/Yamamoto/Clear/CanGoal.cs
@@ -8,25 +8,51 @@ public class CanGoal : MonoBehaviour
 
     [SerializeField] private TestPlayer playerscript;
     [SerializeField] private GameObject goal;
-    [SerializeField] private GameObject check;
+    [SerializeField] private List<GameObject> searched = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //初期化
         tm = TileManager.tm;
         sm = SweetsManager.sm;
+        searched.Clear();
     }
 
     /// <summary>
     /// ゴールできるかを判定する関数
     /// </summary>
     /// <param name="now"></param> 基準となるマスのスクリプト
-    private void CanMassThrough(Tile now, GameObject test)
+    /// お菓子を移動させて残り移動数が0になったときにこの関数を実行する
+    private bool CanMassThrough(Tile now)
     {
-        foreach (KeyValuePair<GameObject, Vector2> pair in tm.tiles)
-        {
+        //検索済みリストに重複があったらreturn
+        if (searched.Contains(now.gameObject)) return false;
 
+        //リストに追加
+        searched.Add(now.gameObject);
+
+        //基準マスの隣接マスを検索
+        foreach (KeyValuePair<GameObject, Vector2> pair in now.neighbor)
+        {
+            //隣接マスにお菓子があったら次のマスへ
+            Sweets sweets = sm.GetSweets(pair.Key.transform.position);
+            if (sweets != null) continue;
+
+            //お菓子のない隣接マスがゴールマスなら
+            if (pair.Key == goal)
+            {
+                //デバッグ
+                //Debug.Log($"serachmass : {now.gameObject.name}  goal : {pair.Key.name}");
+
+                return true;
+            }
+
+            //隣接マスの隣接マスを検索
+            if (CanMassThrough(pair.Key.GetComponent<Tile>())) return true;
         }
+
+        return false;
     }
 
     // Update is called once per frame
@@ -35,8 +61,7 @@ public class CanGoal : MonoBehaviour
         //テスト用
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            GameObject ob = Instantiate(check);
-            CanMassThrough(playerscript.ReturnNowTileScript(), ob);
+            Debug.Log($"serach is end : {CanMassThrough(playerscript.ReturnNowTileScript())}");
         }
     }
 }
