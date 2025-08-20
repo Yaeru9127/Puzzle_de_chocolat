a38e9cd4dc11ba13235043639f1086331473b83e
中島壮真 <ktc25a31a0038@edu.kyoto-tech.ac.jp>
2025-08-20 12:34:43 +0900

    Option画面押した後ボタン移動操作　追加　

--- Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
+++ Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
@@ -1,64 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
+using System.Collections.Generic;
 
 public class UIButtonOutlineSelector : MonoBehaviour
 {
-    // このオブジェクトの子供にあるButtonコンポーネントを全て格納する配列
-    private UnityEngine.UI.Button[] buttons;
+    // このオブジェクトの子供にあるButtonコンポーネントを全て格納するリスト
+    private List<Button> buttons = new List<Button>();
 
-    void Start()
+    void Start()    
     {
         // 子オブジェクトからButtonコンポーネントを全て取得
-        buttons = GetComponentsInChildren<UnityEngine.UI.Button>();
+        buttons.AddRange(GetComponentsInChildren<Button>());
 
         // ボタンが1つ以上あれば、最初のボタンを選択状態にしてアウトラインを有効にする
-        if (buttons.Length > 0)
+        if (buttons.Count > 0)
         {
-            // EventSystemに最初のボタンを選択状態として登録
             EventSystem.current.SetSelectedGameObject(buttons[0].gameObject);
-
-            // 最初のボタンにアウトラインを付ける
             SetOutline(buttons[0], true);
         }
     }
 
     void Update()
     {
-        // 現在選択されているUIオブジェクトを取得
         var selectedObj = EventSystem.current.currentSelectedGameObject;
 
-        // 全ボタンに対して処理を行う
         foreach (var btn in buttons)
         {
-            // 現在選択されているボタンかどうかを判定
             bool isSelected = (btn.gameObject == selectedObj);
-
-            // 選択されているボタンにはアウトラインを有効にし、それ以外は無効にする
             SetOutline(btn, isSelected);
         }
     }
 
-    // 指定したボタンにアウトラインを付けたり外したりする関数
-    void SetOutline(UnityEngine.UI.Button btn, bool enable)
+    // オプションメニューのボタンを追加登録する関数
+    public void AddOptionButtons(GameObject optionMenuRoot)
     {
-        // ボタンに既にOutlineコンポーネントがあるか調べる
+        Button[] optionButtons = optionMenuRoot.GetComponentsInChildren<Button>(true); // 非アクティブでも取得
+        foreach (var btn in optionButtons)
+        {
+            if (!buttons.Contains(btn)) // 重複登録を防ぐ
+            {
+                buttons.Add(btn);
+            }
+        }
+    }
+
+    void SetOutline(Button btn, bool enable)
+    {
         Outline outline = btn.GetComponent<Outline>();
 
         if (outline == null)
         {
-            // Outlineが無ければ追加する
             outline = btn.gameObject.AddComponent<Outline>();
-
-            // アウトラインの色を赤に設定
             outline.effectColor = Color.red;
-
-            // アウトラインの幅を調整
             outline.effectDistance = new Vector2(3, 3);
         }
 
-        // アウトラインの表示・非表示を切り替え
         outline.enabled = enable;
     }
 }
