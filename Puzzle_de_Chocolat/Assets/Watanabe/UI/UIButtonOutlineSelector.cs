c02327ffb48ee3dcc71dd6a0c4f484c95c9d0e8f
３ <>
2025-08-22 19:48:20 +0900

    BGMボタン移動　途中

--- Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
+++ Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
@@ -5,57 +5,79 @@ using System.Collections.Generic;
 
 public class UIButtonOutlineSelector : MonoBehaviour
 {
-    // このオブジェクトの子供にあるButtonコンポーネントを全て格納するリスト
+    // 子オブジェクト内のButtonをまとめて管理するリスト
     private List<Button> buttons = new List<Button>();
 
-    void Start()    
+    // SliderとTextをペアで管理するクラス
+    [System.Serializable]
+    public class SliderLabelPair
     {
-        // 子オブジェクトからButtonコンポーネントを全て取得
+        public Slider slider;   // BGM / SE のスライダー
+        public Text label;      // スライダーの横にあるラベルText
+    }
+
+    // Inspector上で設定できるようにSerializeField化
+    [SerializeField] private List<SliderLabelPair> sliderLabelPairs = new List<SliderLabelPair>();
+
+    void Start()
+    {
+        // 子オブジェクトにある全てのButtonを登録
         buttons.AddRange(GetComponentsInChildren<Button>());
 
-       
+        // 最初にボタンが存在すれば、そのボタンを選択状態にする
         if (buttons.Count > 0)
         {
             EventSystem.current.SetSelectedGameObject(buttons[0].gameObject);
-            SetOutline(buttons[0], true);
+            SetOutline(buttons[0], true);  // 最初のボタンに赤枠を付ける
         }
     }
 
     void Update()
     {
+        // 現在EventSystemで選択されているUIオブジェクトを取得
         var selectedObj = EventSystem.current.currentSelectedGameObject;
 
+        // ① ボタンの選択判定
         foreach (var btn in buttons)
         {
             bool isSelected = (btn.gameObject == selectedObj);
-            SetOutline(btn, isSelected);
+            SetOutline(btn, isSelected);   
         }
-    }
 
-    // オプションメニューのボタンを追加登録する関数
-    public void AddOptionButtons(GameObject optionMenuRoot)
-    {
-        Button[] optionButtons = optionMenuRoot.GetComponentsInChildren<Button>(true); 
-        foreach (var btn in optionButtons)
+        // ② スライダーの選択判定
+        foreach (var pair in sliderLabelPairs)
         {
-            if (!buttons.Contains(btn)) 
+            bool isSelected = (pair.slider != null && pair.slider.gameObject == selectedObj);
+            if (pair.label != null)
             {
-                buttons.Add(btn);
+                SetOutline(pair.label, isSelected);
             }
         }
     }
 
+    // Buttonに赤枠を設定する
     void SetOutline(Button btn, bool enable)
     {
         Outline outline = btn.GetComponent<Outline>();
-
         if (outline == null)
         {
             outline = btn.gameObject.AddComponent<Outline>();
-            outline.effectColor = Color.red;
-            outline.effectDistance = new Vector2(3, 3);
+            outline.effectColor = Color.red;          // 赤色の枠
+            outline.effectDistance = new Vector2(3, 3); // 枠線の太さ
         }
+        outline.enabled = enable;
+    }
 
+    // Textに赤枠を設定する
+    void SetOutline(Text txt, bool enable)
+    {
+        Outline outline = txt.GetComponent<Outline>();
+        if (outline == null)
+        {
+            outline = txt.gameObject.AddComponent<Outline>();
+            outline.effectColor = Color.red;          // 赤色の枠
+            outline.effectDistance = new Vector2(1, 1); // 枠線の太さ
+        }
         outline.enabled = enable;
     }
 }
