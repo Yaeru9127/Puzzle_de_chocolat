aadc6808f514017e1917f3ecc0149586278e8699
中島壮真 <ktc25a31a0038@edu.kyoto-tech.ac.jp>
2025-08-27 12:34:17 +0900

    オプションからタイトル　途中

--- Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
+++ Puzzle_de_Chocolat/Assets/Watanabe/UI/UIButtonOutlineSelector.cs
@@ -5,10 +5,8 @@ using System.Collections.Generic;
 
 public class UIButtonOutlineSelector : MonoBehaviour
 {
-    // 子オブジェクト内のButtonをまとめて管理するリスト
     private List<Button> buttons = new List<Button>();
 
-    // SliderとTextをペアで管理するクラス
     [System.Serializable]
     public class SliderLabelPair
     {
@@ -16,32 +14,33 @@ public class UIButtonOutlineSelector : MonoBehaviour
         public Text label;      // スライダーの横にあるラベルText
     }
 
-    // Inspector上で設定できるようにSerializeField化
-    [SerializeField] private List<SliderLabelPair> sliderLabelPairs = new List<SliderLabelPair>();
+    [SerializeField]
+    private List<SliderLabelPair> sliderLabelPairs = new List<SliderLabelPair>();
 
     void Start()
     {
         // 子オブジェクトにある全てのButtonを登録
         buttons.AddRange(GetComponentsInChildren<Button>());
 
-        // 最初にボタンが存在すれば、そのボタンを選択状態にする
-        if (buttons.Count > 0)
-        {
-            EventSystem.current.SetSelectedGameObject(buttons[0].gameObject);
-            SetOutline(buttons[0], true);  // 最初のボタンに赤枠を付ける
-        }
+        // 最初の選択を設定
+        SelectFirst();
+    }
+
+    void OnEnable()
+    {
+        // シーン切替や再表示時にも必ず最初を選択する
+        SelectFirst();
     }
 
     void Update()
     {
-        // 現在EventSystemで選択されているUIオブジェクトを取得
         var selectedObj = EventSystem.current.currentSelectedGameObject;
 
         // ① ボタンの選択判定
         foreach (var btn in buttons)
         {
             bool isSelected = (btn.gameObject == selectedObj);
-            SetOutline(btn, isSelected);   
+            SetOutline(btn, isSelected);
         }
 
         // ② スライダーの選択判定
@@ -50,33 +49,43 @@ public class UIButtonOutlineSelector : MonoBehaviour
             bool isSelected = (pair.slider != null && pair.slider.gameObject == selectedObj);
             if (pair.label != null)
             {
+                // ラベル自体は選択されないけど、見た目で枠を出す
                 SetOutline(pair.label, isSelected);
             }
         }
     }
 
-    // Buttonに赤枠を設定する
+    // 最初のボタンを選択状態にする
+    private void SelectFirst()
+    {
+        if (buttons.Count > 0 && EventSystem.current != null)
+        {
+            EventSystem.current.SetSelectedGameObject(null); // いったんリセット
+            EventSystem.current.SetSelectedGameObject(buttons[0].gameObject);
+            SetOutline(buttons[0], true);
+        }
+    }
+
     void SetOutline(Button btn, bool enable)
     {
         Outline outline = btn.GetComponent<Outline>();
         if (outline == null)
         {
             outline = btn.gameObject.AddComponent<Outline>();
-            outline.effectColor = Color.red;          // 赤色の枠
-            outline.effectDistance = new Vector2(3, 3); // 枠線の太さ
+            outline.effectColor = Color.red;
+            outline.effectDistance = new Vector2(3, 3);
         }
         outline.enabled = enable;
     }
 
-    // Textに赤枠を設定する
     void SetOutline(Text txt, bool enable)
     {
         Outline outline = txt.GetComponent<Outline>();
         if (outline == null)
         {
             outline = txt.gameObject.AddComponent<Outline>();
-            outline.effectColor = Color.red;          // 赤色の枠
-            outline.effectDistance = new Vector2(1, 1); // 枠線の太さ
+            outline.effectColor = Color.red;
+            outline.effectDistance = new Vector2(1, 1);
         }
         outline.enabled = enable;
     }
